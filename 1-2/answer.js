16e34f5ffc93b6cb17d757afe2bf6c375dbdcbb8
Joren Broekema <joren.broekema@gmail.com>
2019-12-04 21:51:21 +0100

    Day 1-2: some recursion already huh

---
+++ 1-2/answer.js
@@ -0,0 +1,23 @@
+import data from '../1-1/data.js';
+
+const calcFuel = mass => {
+  let sum = 0;
+
+  // Inner recursive function, so we can keep track of our sum outside of it
+  const _calcFuel = _mass => {
+    const fuel = Math.floor(_mass / 3) - 2;
+    if (fuel > 0) {
+      sum += fuel;
+      return _calcFuel(fuel);
+    }
+  };
+  _calcFuel(mass);
+
+  return sum;
+};
+
+const totalFuel = data.reduce((acc, value) => {
+  return acc + calcFuel(value);
+}, 0);
+
+console.log(totalFuel);
